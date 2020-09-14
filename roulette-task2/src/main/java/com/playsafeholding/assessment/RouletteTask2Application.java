13ae23ff2c1f4527304f52bfe86522c264034676
Karthik Reddy Perla <perlak@telkom.co.za>
2020-09-14 13:23:32 +0200

    Added model classes and main class

---
+++ roulette-task2/src/main/java/com/playsafeholding/assessment/RouletteTask2Application.java
@@ -0,0 +1,18 @@
+package com.playsafeholding.assessment;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SpringBootApplication
+public class RouletteTask2Application  {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RouletteTask2Application.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting roulette application");
+		SpringApplication.run(RouletteTask2Application.class, args);
+		
+	}
+
+}
