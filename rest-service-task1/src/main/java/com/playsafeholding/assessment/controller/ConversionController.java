787649ad9d44856c8af3664ae3d4fc3584c6991b
Karthik Reddy Perla <perlak@telkom.co.za>
2020-09-14 12:33:37 +0200

    Added controller and updated pom.xml

---
+++ rest-service-task1/src/main/java/com/playsafeholding/assessment/controller/ConversionController.java
@@ -0,0 +1,73 @@
+package com.playsafeholding.assessment.controller;
+
+import java.math.BigDecimal;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.playsafeholding.assessment.dto.Distance;
+import com.playsafeholding.assessment.dto.Temperature;
+
+/**
+ * @author karthik
+ *
+ */
+
+@RestController
+public class ConversionController {
+	
+		/** convert kelvin to celcius
+		 * 
+		 * @param kelvin
+		 * @return celcius
+		 */
+
+		@GetMapping("conversions/ktoc")
+		public Temperature kelvinToCelsius(@RequestParam(value = "kelvin", defaultValue = "0") String kelvin) {
+			Temperature temp = new Temperature();
+		
+			if (NumberUtils.isCreatable(kelvin)) {
+				temp.setKelvin(new BigDecimal(kelvin));
+				return temp;
+			}
+			temp.setKelvin(new BigDecimal("0"));
+			return temp;
+		}
+
+		/** convert celcius to kelvin
+		 * 
+		 * @param
+		 * @return kelvin and celcius
+		 */
+		
+		@GetMapping("conversions/ctok")
+		public Temperature celsiusToKelvin(@RequestParam(value = "celsius", defaultValue = "0") String celsius) {
+			Temperature temp = new Temperature();
+			if (NumberUtils.isCreatable(celsius)) {
+				temp.setCelsius(new BigDecimal(celsius));
+				return temp;
+			}
+			temp.setCelsius(new BigDecimal("0"));
+			return temp;
+		}
+		
+		/** convert miles to kilometers 
+		 * 
+		 * @param miles
+		 * @return kilometers
+		 */
+
+		@GetMapping("conversions/mtok")
+		public Distance milesToKilometers(@RequestParam(value = "miles", defaultValue = "0") String miles) {
+			Distance dist = new Distance();
+			if (NumberUtils.isCreatable(miles)) {
+				dist.setMiles(new BigDecimal(miles));
+				return dist;
+			}
+			dist.setMiles(new BigDecimal("0"));
+			return dist;
+		}
+
+
+
+}
---
+++ rest-service-task1/src/test/java/com/playsafeholding/assessment/RestServiceTask1ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.playsafeholding.assessment;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+@WebMvcTest
+class RestServiceTask1ApplicationTests {
+	@Test
+	   public void main() {
+		RestServiceTask1Application.main(new String[] {});
+	   }
+
+}
